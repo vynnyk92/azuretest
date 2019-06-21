c0c9796c8faeb4931a2e9c4bd6febb5094caf8d6
Filipp Vynnykov <ynov07@rambler.ru>
2019-06-21 11:05:39 +0300

    add cloud db

--- AzureTest/Program.cs
+++ AzureTest/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AzureTest.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace AzureTest
@@ -14,7 +17,19 @@ namespace AzureTest
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            MigrateDB(host);
+            host.Run();
+        }
+
+        private static void MigrateDB(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var ctx = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                ctx.Database.Migrate();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
